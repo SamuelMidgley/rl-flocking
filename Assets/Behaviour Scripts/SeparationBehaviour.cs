8f81b00c5b8ed8a6082a83a24d94840ada609e92
SamuelMidgley <80775550+SamuelMidgley@users.noreply.github.com>
2021-07-07 17:02:25 +0100

    Add files via upload

---
+++ Assets/Behaviour Scripts/SeparationBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Separation")]
+public class SeparationBehaviour : FlockBehaviour
+{
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        // if no neighbours, return no adjustment
+        if (context.Count == 0)
+            return Vector2.zero;
+
+        // add all points together and average
+        Vector2 separationMove = Vector2.zero;
+        int nAvoid = 0;
+        foreach (Transform item in context)
+        {
+            if (Vector2.SqrMagnitude(item.localPosition - agent.transform.localPosition) < flock.SquareAvoidanceRadius)
+            {
+                nAvoid++;
+                separationMove += (Vector2)(agent.transform.localPosition - item.localPosition);
+            } 
+        }
+        if (nAvoid > 0)
+            separationMove /= nAvoid;
+
+        return separationMove;
+    }
+}
---
+++ Assets/MLShepherd.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MLShepherd : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    public static List<string> resetNames = new List<string>();
+
+    public override void OnEpisodeBegin()
+    {
+        resetNames.Add(transform.parent.name);
+        transform.localPosition = new Vector3(-9f, 0f);
+    }
+
+    private void Update()
+    {
+        // Heading right direction
+        // USE LOCAL POSITION
+
+        // collect all agents headings and average
+        //Vector3 avgHeading = Vector3.zero;
+
+        //for (int i = 0; i < Flock.agents.Count; i++)
+        //{
+        //    avgHeading += Flock.agents[i].transform.up;
+        //}
+
+        //avgHeading /= Flock.agents.Count;
+
+        // check if direction would land in the pen
+        // use avg position and heading 
+        // angle between avg pos at top and bottom of pen 
+        // angle of heading, if angle of heading inbetween these give reward 
+        // maybe give percentage of reward if kinda close (NOT SURE ABOUT THIS THOUGH)
+
+        // SerializeField and add the size of the pen so it changes depending on the size of the gameobject
+
+
+        // Circle thing
+        // find centre point of flock
+        // loop through distances from centre point to flock objects
+        // function that rewards a smaller radius
+
+        // Small reward for herdining one sheep but as percentage
+        // End episode if no sheep around
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+
+        float moveSpeed = 8f;
+        transform.localPosition += new Vector3(moveX, moveY) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxis("Horizontal");
+        continuousActions[1] = Input.GetAxis("Vertical");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(+1f);
+            EndEpisode();
+        }
+
+        if (collision.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+    }
+}
